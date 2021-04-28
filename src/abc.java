d37f46d9ab4940e22401e56e8f9fd568daede333
Nandini Agarwal <52369923+nandini-agarwal@users.noreply.github.com>
2021-04-28 16:25:16 +0530

    Add files via upload

---
+++ src/abc.java
@@ -0,0 +1,74 @@
+public class abc {
+}
+
+
+class user(){
+
+    addUser(){
+
+    }
+
+    deleteUser(){
+
+    }
+
+    modifyUser(){
+
+    }
+
+    showUser(){
+
+    }
+
+}
+
+class Reservation(){
+
+    bookReservation(){
+
+    }
+
+    deleteReservation(){
+
+    }
+
+    modifyReservation(){
+
+    }
+
+    showReservation(){
+
+    }
+
+    paymentReservation(){
+
+    }
+
+}
+
+class Trains(){
+    addTrain(){
+
+    }
+
+    cancelTrain(){
+
+    }
+
+    modifyTrain(){
+
+    }
+
+    showTrain(){
+
+    }
+
+    searchTrain(){
+
+    }
+}
+
+    public static void main(String[] args) {
+
+
+    }
